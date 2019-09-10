6537eefdbcb9b81a20df1fae95d147a14c905b05
程林 <chenglin@chenglindeMacBook-Pro.local>
2019-09-10 17:09:18 +0800

    update

--- src/components/list/index.vue
+++ src/components/list/index.vue
@@ -5,20 +5,28 @@
             border
             style="width: 100%">
             <template v-for="item in colunmns">
-                <el-table-column v-if="!item.type" :key="item.dataIndex" :label="item.title" :prop="item.dataIndex">
-                </el-table-column>
-                <el-table-column v-else :key="item.dataIndex"
+                <el-table-column v-if="item.type==='actionBtn'" :key="item.dataIndex"
                     :label="item.title">
                     <template slot-scope="scope">
                         <el-button v-for="ele in item.actionBtns" :key="ele.id" @click="ele.clickFun(scope.row)" type="text" size="small">{{ele.name}}</el-button>
                     </template>
                 </el-table-column>
+                <el-table-column v-else-if="item.type==='selfComponent'" :key="item.dataIndex"
+                    :label="item.title">
+                    <template slot-scope="scope">
+                        <component :is="getComponentName(scope.row)" :row="scope.row"></component>
+                    </template>
+                </el-table-column>
+                <el-table-column v-else :key="item.dataIndex" :label="item.title" :prop="item.dataIndex">
+                </el-table-column>
+                
             </template>
         </el-table>
     </div>
 </template>
 
 <script>
+import Vue from 'vue'
 
 export default {
     name: 'CommonList',
@@ -45,7 +53,7 @@ export default {
                 dataIndex:'zip'
             },{
                 title:'操作',
-                type:'edit',
+                type:'actionBtn',
                 actionBtns:[{
                     id:'edit',
                     name:'编辑',
@@ -53,8 +61,13 @@ export default {
                         console.log(item)
                     }
                 }]
+            },{
+                title:'操作自定义',
+                type:'selfComponent',
+                template:'<div>{{row.name}}</div>',
             }],
             tableData: [{
+                id:1,
                 date: '2016-05-02',
                 name: '王小虎',
                 province: '上海',
@@ -62,6 +75,7 @@ export default {
                 address: '上海市普陀区金沙江路 1518 弄',
                 zip: 200333
             }, {
+                id:2,
                 date: '2016-05-04',
                 name: '王小虎',
                 province: '上海',
@@ -69,6 +83,7 @@ export default {
                 address: '上海市普陀区金沙江路 1517 弄',
                 zip: 200333
             }, {
+                id:3,
                 date: '2016-05-01',
                 name: '王小虎',
                 province: '上海',
@@ -76,6 +91,7 @@ export default {
                 address: '上海市普陀区金沙江路 1519 弄',
                 zip: 200333
             }, {
+                id:4,
                 date: '2016-05-03',
                 name: '王小虎',
                 province: '上海',
@@ -88,11 +104,23 @@ export default {
     computed: {
         
     },
-    mounted(){
-        
+    created(){
+        let tableData = this.tableData;
+        tableData.forEach((ele)=>{
+            ele.template = '<div>{{row.name}}-{{row.id}}</div>'
+        })
+        for (let ele of tableData) {
+            Vue.component(`cmp-${ele.id}`, {
+                props: ['row'],
+                template: ele.template
+            })
+        }
+        this.tableData = tableData
     },
     methods: {
-        
+        getComponentName (row) {
+            return `cmp-${row.id}`
+        }
     }
 }
 </script>
