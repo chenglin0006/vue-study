d36b50a31a8f0ec15251fead9d0fa8465c19a75f
程林 <chenglin@chenglindeMacBook-Pro.local>
2019-09-17 16:48:24 +0800

    lazy loader

--- src/router/routerConfig.js
+++ src/router/routerConfig.js
@@ -1,9 +1,16 @@
-import HelloWorld from '@/page/hello/HelloWorld'
-import Test from '@/page/test/Test'
-import BrandList from '@/page/brand/BrandList'
-import NewBrand from '@/page/brand/NewBrand'
-import SlotExample from '@/page/slot/SlotExample'
-import Example from '@/page/example/Example'
+// import HelloWorld from '@/page/hello/HelloWorld'
+// import Test from '@/page/test/Test'
+// import BrandList from '@/page/brand/BrandList'
+// import NewBrand from '@/page/brand/NewBrand'
+// import SlotExample from '@/page/slot/SlotExample'
+// import Example from '@/page/example/Example'
+
+const HelloWorld = () => import('@/page/hello/HelloWorld')
+const Test = () => import('@/page/test/Test')
+const BrandList = () => import('@/page/brand/BrandList')
+const NewBrand = () => import('@/page/brand/NewBrand')
+const SlotExample = () => import('@/page/slot/SlotExample')
+const Example = () => import('@/page/example/Example')
 
 export default [
     {
